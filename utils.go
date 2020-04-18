c837c4ba97bb89f6ecbfa9562f0c9c03cf2901c8
y-moriwake <moriwake.yutaka@myutaka-engineer.com>
2020-04-18 13:12:03 +0900

    微修正

--- utils.go
+++ utils.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"os"
 )
 
@@ -34,68 +31,4 @@ func logError(args ...interface{}) {
 func logWarning(args ...interface{}) {
 	logger.SetPrefix("WARNING:")
 	logger.Println(args...)
-}
-
-// DB接続サンプル
-func connDbSample(w http.ResponseWriter, r *http.Request) {
-
-	// データベース接続
-	db, err := sql.Open("mysql", "admin:Moriwake1@tcp(moriwake-rds.cfi6vbfhf6p0.ap-northeast-1.rds.amazonaws.com:3306)/photoN?charset=utf8")
-	if err != nil {
-		logError(err.Error())
-	}
-	defer db.Close()
-
-	// 接続確認
-	err = db.Ping()
-	if err != nil {
-		fmt.Fprintf(w, "データベース接続失敗")
-	} else {
-		fmt.Fprintf(w, "データベース接続成功")
-	}
-
-	// クエリ発行
-	rows, err := db.Query("SELECT * FROM users")
-	if err != nil {
-		panic(err.Error())
-	}
-	// カラム名を取得
-	columns, err := rows.Columns()
-	if err != nil {
-		panic(err.Error())
-	}
-
-	values := make([]sql.RawBytes, len(columns))
-
-	scanArgs := make([]interface{}, len(values))
-
-	for i := range values {
-		scanArgs[i] = &values[i]
-	}
-
-	for rows.Next() {
-		err = rows.Scan(scanArgs...)
-		if err != nil {
-			panic(err.Error())
-		}
-		var value string
-		for i, col := range values {
-			// Here we can check if the value is nil (NULL value)
-			if col == nil {
-				value = "NULL"
-			} else {
-				value = string(col)
-			}
-			fmt.Fprintf(w, columns[i], ": ", value)
-		}
-		fmt.Println("-----------------------------------")
-	}
-
-}
-
-// ステータス設定(TODO エラーメッセージ追加)
-func inResponseStatus(w http.ResponseWriter, status int) {
-	// レスポンスヘッダ追加(WriteHeaderの後に記述すると無効)
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.WriteHeader(status)
 }
